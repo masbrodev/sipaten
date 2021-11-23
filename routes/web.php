e8cc224fbae756f13a718663faad507bd2788c69
MASBRO <s2gotosingapore@gmail.com>
2021-11-23 13:45:10 +0700

    install admin LTE

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::resource('bmn', KategoriBmnController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
