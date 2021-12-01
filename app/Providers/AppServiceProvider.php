030bf06b5c485c4398bc40664d8ce31c08488d6d
MASBRO <s2gotosingapore@gmail.com>
2021-12-02 00:54:01 +0700

    Currenncy Function

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        Blade::directive('rp', function ($convert) {
+            return "<?php echo number_format($convert, 0, ', ', '.'); ?>";
+        });
     }
 }
