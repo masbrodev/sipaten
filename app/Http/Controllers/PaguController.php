75cb07494858935bc2509c5b1216f64eed82e232
MASBRO <s2gotosingapore@gmail.com>
2021-12-13 13:48:58 +0700

    Revert "sitik"
    This reverts commit 0c3fa86797304cd975416f6cec1b8f1d4b7d3d29.

--- app/Http/Controllers/PaguController.php
+++ app/Http/Controllers/PaguController.php
@@ -51,7 +51,7 @@ class PaguController extends Controller
                 'nilai' => $pg->nilai,
                 'pagu_anggaran' => $pg->pagu_anggaran,
                 'sisa' => $pg->sisa,
-                'realisasi' =>  0,
+                'realisasi' => in_array($pg->kode_pagu, $ll) ? $transaksi2[$pg->kode_pagu] : null,
                 'diperbaharui' => $pg->updated_at->diffForHumans(),
             ]);
         }
